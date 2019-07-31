fd935fe89e2f3c7c957c7f3f4fc0c1777804aa90
Anastasia Tondera <a.tondera@gmail.com>
2019-07-31 21:02:40 +0200

    issue #8: Fix conan profile for x86_86. Update example

--- example-app/app/src/main/cpp/native-lib.cpp
+++ example-app/app/src/main/cpp/native-lib.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/kdtree/impl/kdtree_flann.hpp>
+#include <pcl/search/impl/kdtree.hpp>
+#include <pcl/search/impl/organized.hpp>
+#include <pcl/filters/impl/statistical_outlier_removal.hpp>
 #include <iostream>
 
 #include <android/log.h>
@@ -9,27 +13,36 @@
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "bashbug.example", __VA_ARGS__))
 
 /*
- * Example: http://pointclouds.org/documentation/tutorials/writing_pcd.php#writing-pcd
+ * Example:
+ * http://pointclouds.org/documentation/tutorials/writing_pcd.php#writing-pcd
+ * http://pointclouds.org/documentation/tutorials/statistical_outlier.php
  */
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_example_bashbug_example_MainActivity_pclExampleFunc(JNIEnv *env, jobject instance) {
 
-    pcl::PointCloud<pcl::PointXYZ> cloud;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
 
     // Fill in the cloud data
-    cloud.width    = 5;
-    cloud.height   = 1;
-    cloud.is_dense = false;
-    cloud.points.resize (cloud.width * cloud.height);
+    cloud->width    = 5;
+    cloud->height   = 1;
+    cloud->is_dense = false;
+    cloud->points.resize(cloud->width * cloud->height);
 
-    for (size_t i = 0; i < cloud.points.size (); ++i)
+    for (size_t i = 0; i < cloud->points.size (); ++i)
     {
-        cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-        cloud.points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
+        cloud->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
+        cloud->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
+        cloud->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
     }
 
-    LOGI("pointcloud has size %lu", cloud.points.size());
+    LOGI("pointcloud has size %lu", cloud->points.size());
+
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+    sor.setInputCloud(cloud);
+    sor.setMeanK(50);
+    sor.setStddevMulThresh (1.0);
+    sor.filter(*cloud_filtered);
 }
